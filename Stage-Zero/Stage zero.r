8a4b467fa8e1f74b3368a1f46ce34ca1fb610327
Valerie-Martins <val4martins@gmail.com>
2025-02-16 22:25:57 +0100

    Create Stage zero.r

---
+++ Stage-Zero/Stage zero.r
@@ -0,0 +1,11 @@
+#R Code
+super_serine_members <- data.frame(
+  Names = c("Samuel Badekale", "Valerie Martins", "Zainab Suleiman", "Uwidia Emmanuel"),
+  Slack_username = c("Samuelbadekale", "Valarie", "Bnt_Suleiman", "Uwidia"),
+  Email = c("samuelbadekale@gmail.com", "val4martins@gmail.com", "suleimanzainab280@gmail.com", "uwidiaosalodion@gmail.com"),
+  Hobby = c("Reading, Watching movies and playing games", "Watching movies", "Reading", "Reading and taking walks"),
+  Country = c("Nigeria", "Nigeria", "Nigeria","Nigeria"),
+  Discipline = c("Cell Biology and Genetics", "Cell and Molecular Biology", "Biochemistry", "Medical Biochemistry"),
+  Preferred_programming_language = c("Python", "Python", "R", "Python")
+)
+print(super_serine_members)
